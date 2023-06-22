d1c2f93210564d2205974863516d86679d801257
danhigg <higgins.daniel@gmail.com>
2023-06-22 15:21:51 +1000

    shape event flow and game over condition

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+//define references to HTML elements
+let choiceContainer = document.querySelector("#quest-container"); // identify area to create choice buttons
+let headerQuest = document.querySelector("#header-quest"); // identify area to insert question prompt
+
 // create a template for creating questions
 let defineQuest = function(questPrompt, choice1, choice2, choice3, choice4, questAnswer) {
     this.questPrompt = questPrompt;
@@ -13,43 +17,70 @@ let quizQuest2 = new defineQuest("what1", "who1", "when1", "where1", "why1", "wh
 let quizQuest3 = new defineQuest("what2", "who2", "when2", "where2", "why2", "where2");
 let quizQuest4 = new defineQuest("what3", "who3", "when3", "where3", "why3", "why3");
 
-console.log(quizQuest1.choice1);
-console.log(quizQuest1.choice2 === quizQuest1.questAnswer);
-
 // define the set of all questions to be asked
 let questions = [quizQuest1, quizQuest2, quizQuest3, quizQuest4];
+
+// define inital values on page 
+let questionIndex = 0;
 let currentQuest = questions[0];
 
-console.log(currentQuest.questPrompt);
-console.log(currentQuest.choice2 === currentQuest.questAnswer);
+// start/continue question prompts
+let advanceQuest = function() {
+currentQuest = questions[questionIndex];
+askQuest(currentQuest.questPrompt);
+createButtons(currentQuest.choice1, currentQuest.choice2, currentQuest.choice3, currentQuest.choice4);
+}
 
+// advance question index and check if the quiz is over after each choice is made
+let isGameOver = function() {
+    questionIndex ++;
+    console.log(questionIndex);
+    if (questionIndex === questions.length) {
+        gameOver();
+    } else {
+        advanceQuest();
+    }
+}
 
-// identify area to create choice buttons
-let questContainer = document.querySelector("#quest-container");
-// insert a button for each choice
+// define after quiz event flow
+let gameOver = function() {
+    headerQuest.innerText = "";
+    choiceContainer.innerHTML = "";
+    console.log("game over!");
+}
+
+// function to isert the question prompt
+let askQuest  = function(questText) {
+    headerQuest.innerText = questText
+}
+
+// function to insert a button for each choice
 let createButtons = function(button1, button2, button3, button4) {
+    choiceContainer.innerHTML = "";
     // TODO: create for loop
     let insButton1 = document.createElement("button");
     insButton1.innerText = button1;
-    questContainer.appendChild(insButton1);
+    choiceContainer.appendChild(insButton1);
     let insButton2 = document.createElement("button");
     insButton2.innerText = button2;
-    questContainer.appendChild(insButton2);
+    choiceContainer.appendChild(insButton2);
     let insButton3 = document.createElement("button");
     insButton3.innerText = button3;
-    questContainer.appendChild(insButton3);
+    choiceContainer.appendChild(insButton3);
     let insButton4 = document.createElement("button");
     insButton4.innerText = button4;
-    questContainer.appendChild(insButton4);
+    choiceContainer.appendChild(insButton4);
  }
- createButtons(currentQuest.choice1, currentQuest.choice2, currentQuest.choice3, currentQuest.choice4);
+ advanceQuest();
 
-
-// Answer verification
-questContainer.addEventListener("click", function(event) {
+// Answer verification using an event listener
+choiceContainer.addEventListener("click", function(event) {
     if (event.target.innerText === currentQuest.questAnswer) {
         console.log("Correct!");
+        isGameOver();
+        
     } else {
         console.log("Wrong!");
+        isGameOver();
     }
 });
